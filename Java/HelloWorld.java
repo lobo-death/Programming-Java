34991bba4dcf06ef4e96f045fa8dda00aa6db4ab
MARCELO MACHADO PEREIRA <mpereira@hbb.local>
2016-09-22 11:24:10 -0300

    First commit

---
+++ Java/HelloWorld.java
@@ -0,0 +1,15 @@
+
+/**
+ * Imprime na tela uma saudação "Hello World".
+ * 
+ * @author (Marcelo Machado Pereira) 
+ * @version (22-09-2016)
+ */
+public class HelloWorld{
+    /**
+     * COnstrutor para objetos da classe HelloWorld
+     */
+    public HelloWorld(){
+        System.out.println("Hello World!");
+    }
+}
